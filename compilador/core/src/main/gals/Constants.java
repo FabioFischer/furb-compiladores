052a9f18f595f33038f11b22ac216028feea4c4c
Fábio L. Fischer <feuture@hotmail.com>
2018-09-24 19:07:42 -0300

    Código gerado pelo gals

---
+++ compilador/core/src/main/gals/Constants.java
@@ -0,0 +1,44 @@
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_palavra_reservada = 2;
+    int t_identificador = 3;
+    int t_constante_numerica = 4;
+    int t_constante_caractere = 5;
+    int t_constante_literal = 6;
+    int t_comentario_linha = 7;
+    int t_and = 8;
+    int t_ARRAY = 9;
+    int t_DO = 10;
+    int t_EXIT = 11;
+    int t_FALSE = 12;
+    int t_IF = 13;
+    int t_IFFALSEDO = 14;
+    int t_IFTRUEDO = 15;
+    int t_LITERAL = 16;
+    int t_LOGICAL = 17;
+    int t_NUMBER = 18;
+    int t_NOT = 19;
+    int t_OR = 20;
+    int t_READ = 21;
+    int t_TRUE = 22;
+    int t_WRITE = 23;
+    int t_TOKEN_24 = 24; //"("
+    int t_TOKEN_25 = 25; //")"
+    int t_TOKEN_26 = 26; //"["
+    int t_TOKEN_27 = 27; //"]"
+    int t_TOKEN_28 = 28; //"=="
+    int t_TOKEN_29 = 29; //"<>"
+    int t_TOKEN_30 = 30; //"<"
+    int t_TOKEN_31 = 31; //"<="
+    int t_TOKEN_32 = 32; //">"
+    int t_TOKEN_33 = 33; //">="
+    int t_TOKEN_34 = 34; //"+"
+    int t_TOKEN_35 = 35; //"-"
+    int t_TOKEN_36 = 36; //"*"
+    int t_TOKEN_37 = 37; //"/"
+    int t_TOKEN_38 = 38; //"'"
+
+}
